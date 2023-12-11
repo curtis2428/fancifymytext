4ad7469de27a595f7c2327ddc9554547a6d085ca
curtis2428 <114844835+curtis2428@users.noreply.github.com>
2023-12-10 20:16:23 -0800

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,32 @@
+function showHelloWorldAlert() {
+    alert('Hello, world!');
+}
+
+function makeTextBigger() {
+    document.getElementById("textArea").style.fontSize = "24pt";
+}
+
+function addSomeStyle(){
+    //alert("This shit work?")
+    document.getElementById("textArea").style.fontWeight = "bold";
+    document.getElementById("textArea").style.color = "blue";
+    document.getElementById("textArea").style.textDecoration = "underline";
+}
+function takeAwayStyle(){
+    document.getElementById("textArea").style.fontWeight = "normal";
+}
+
+function toUpperCase(){
+    let textArea = document.getElementById("textArea");
+    let text = textArea.value.toUpperCase();
+
+    let sentences = text.split(".");
+    for (var i = 0; i < sentences.length; i++) {
+        if (sentences[i].trim().length > 0) {
+            sentences[i] = sentences[i].trim() + "-Moo";
+        }
+    }
+
+    textArea.value = sentences.join(". ") + (text.endsWith(".") ? "" : ".");
+
+}
